b0a72298bd11e02a8ff82a9070f8d8ab7758654e
Arash Khoeini <arash.khoeini@gmail.com>
2023-03-30 20:23:00 -0700

    change Unet architecture

--- models/unet.py
+++ models/unet.py
@@ -224,7 +224,7 @@ def up_conv(in_channels, out_channels):
 
 
 class UNetVGG16(nn.Module):
-    def __init__(self, encoder= None, out_channels=12):
+    def __init__(self, encoder= None, n_classes):
         super().__init__()
 
         if encoder is None:
@@ -234,65 +234,56 @@ class UNetVGG16(nn.Module):
 
         self.block1 = nn.Sequential(*self.encoder[:6])
         self.block2 = nn.Sequential(*self.encoder[6:13])
-        self.block3 = nn.Sequential(*self.encoder[13:20])
-        self.block4 = nn.Sequential(*self.encoder[20:27])
-        self.block5 = nn.Sequential(*self.encoder[27:34])
+        self.block3 = nn.Sequential(*self.encoder[13:23])
+        self.block4 = nn.Sequential(*self.encoder[23:33])
 
 
-        self.bottleneck = nn.Sequential(*self.encoder[34:])
+        self.bottleneck = nn.Sequential(*self.encoder[33:])
         self.conv_bottleneck = conv(512, 1024)
 
 
         self.up_conv6 = up_conv(1024, 512)
         self.conv6 = conv(512 + 512, 512)
         self.up_conv7 = up_conv(512, 256)
-        self.conv7 = conv(256 + 512, 256)
+        self.conv7 = conv(256 + 256, 256)
         self.up_conv8 = up_conv(256, 128)
-        self.conv8 = conv(128 + 256, 128)
+        self.conv8 = conv(128 + 128, 128)
         self.up_conv9 = up_conv(128, 64)
-        self.conv9 = conv(64 + 128, 64)
-        self.up_conv10 = up_conv(64, 32)
-        self.conv10 = conv(32 + 64, 32)
-        self.conv11 = nn.Conv2d(32, out_channels, kernel_size=1)
+        self.conv9 = conv(64 + 64, 32)
+        self.conv_final = nn.Conv2d(32, n_classes, kernel_size=1)
         
     def forward(self, x):
         block1 = self.block1(x)
         block2 = self.block2(block1)
         block3 = self.block3(block2)
         block4 = self.block4(block3)
-        block5 = self.block5(block4)
 
 
-        bottleneck = self.bottleneck(block5)
+        bottleneck = self.bottleneck(block4)
         x = self.conv_bottleneck(bottleneck)
 
 
         x = self.up_conv6(x)
-        x = torch.cat([x, block5], dim=1)
+        x = torch.cat([x, block4], dim=1)
         x = self.conv6(x)
 
 
         x = self.up_conv7(x)
-        x = torch.cat([x, block4], dim=1)
+        x = torch.cat([x, block3], dim=1)
         x = self.conv7(x)
 
 
         x = self.up_conv8(x)
-        x = torch.cat([x, block3], dim=1)
+        x = torch.cat([x, block2], dim=1)
         x = self.conv8(x)
 
 
         x = self.up_conv9(x)
-        x = torch.cat([x, block2], dim=1)
+        x = torch.cat([x, block1], dim=1)
         x = self.conv9(x)
 
 
-        x = self.up_conv10(x)
-        x = torch.cat([x, block1], dim=1)
-        x = self.conv10(x)
-
-
-        x = self.conv11(x)
+        x = self.conv_final(x)
 
 
         return x
